1bb24a119e82bac8232f22d4d557907b6d36e658
Ivan Grech <b42952@mcast.org.mt>
2017-10-11 10:36:00 +0200

    Isma din il commit xfiha? NumberWizard Complete

---
+++ Lesson2/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+    //creates 3 number variables
+    int min = 1;
+    int max = 1000;
+    int guess = 500;
+
+    void NextGuess()
+    {
+        print("Is the number greater or smaller than " + guess + "?");
+        print("UP: Greater DOWN: Smaller ENTER: Equal");
+    }
+
+    void StartGame()
+    {
+        //prints once in the console
+        //print and Debug.Log is the same
+        print("Welcome to Number Wizard");
+        Debug.Log("Please choose a number in your head between " + min + " and " + max);
+        //calling method NextGuess()
+        NextGuess();
+    }
+    // Use this for initialization
+    void Start() {
+        StartGame();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //if the input from the keyboard is up arrow
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("UP Arrow Pressed");
+            min = guess;
+            guess = (min + max) / 2;
+            NextGuess();
+        }
+
+        else
+
+        //if the input from the keyboard is down arrow
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("DOWN Arrow Pressed");
+            max = guess;
+            guess = (min + max) / 2;
+            NextGuess();
+        }
+
+        else
+
+        //if the input from the keyboard is enter
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("ENTER Pressed");
+            print("I GUESSED IT!! The Number was " +guess+" :)");
+        }
+    }
+}
